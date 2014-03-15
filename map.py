e740deae9f51728b407272fa9047cf798c3aa55e
Gareth Paul Jones <gareth@garethpaul.com>
2014-03-15 12:10:04 +0000

    adding reverse geocode supplying location

--- map.py
+++ map.py
@@ -6,6 +6,16 @@ import json
 import const
 from base import Base
 
+
+def get_address(lat,lng):
+	"""
+	Utilize Google's Geocode API to return an approximate address
+	"""
+	geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + str(lat) + "," + str(lng) + "&key=" + const.geocode_key + "&sensor=true"
+	result = urllib2.urlopen(geocode_url).read()
+	results = json.loads(result)['results']
+	return results[0]['formatted_address']
+
 class ApiHandler(Base):
 	def get(self):
 		result = urllib2.urlopen(const.map_api).read()
@@ -16,4 +26,5 @@ class ApiHandler(Base):
 		lat = "{0:.2f}".format(float(lat))
 		lng = "{0:.2f}".format(float(lng))
 		self.response.headers['Content-Type'] = 'application/json'
-		self.response.out.write(json.dumps({"lat": lat, "lng": lng}))
+		address = get_address(lat, lng)
+		self.response.out.write(json.dumps({"lat": lat, "lng": lng, "address": address}))
