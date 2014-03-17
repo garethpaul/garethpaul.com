104c500986c1efde4955bb008263d79d7bc9ab89
Gareth Paul Jones <gareth@garethpaul.com>
2014-03-17 17:07:50 +0000

    adding some notes to glass.py

--- glass.py
+++ glass.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
+"""
+Main Handler for GoogleGlass
+
+There is an API that provides a list of images from Glass to be used specifically for this user.
+Note:
+  const.py provides the HTTPS endpoint.
+
+Todo:
+  create a cache service to limit HTTP requests to the server.
+
+"""
 import main
 import urllib2
 import json
@@ -8,6 +19,7 @@ from base import Base
 
 class GlassHandler(Base):
   def get(self):
+    """ work as a proxy for the glass images api """
     result = urllib2.urlopen(const.glass_api).read()
     self.response.headers['Content-Type'] = 'application/json'
     data = json.loads(result)
